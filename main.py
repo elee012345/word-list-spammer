3aca5397a447d66b035d8394d44ef065869a7093
ethan <ethanlee012345@gmail.com>
2023-05-05 21:17:48 -0500

    button binded to side mouse button, alternating case
    bye bye unneeded code

--- main.py
+++ main.py
@@ -1,43 +1,18 @@
-from pynput import keyboard
 from pynput.keyboard import Key, Controller
-import pynput
 from pynput import mouse
 import time
 
 keybind_pressed = False
 
-# def on_press(key):
-#     # try:
-#     #     print('alphanumeric key {0} pressed'.format(key.char))
-#     # except AttributeError:
-#     #     print('special key {0} pressed'.format(key))
-#     if ( key == keyboard.Key.shift_r ):
-#         global keybind_pressed
-#         keybind_pressed = True
-# 
-#     
-# 
-# def on_release(key):
-#     #print('{0} released'.format(key))
-#     global keybind_pressed
-#     keybind_pressed = False
-# 
-# # ...or, in a non-blocking fashion:
-# listener = keyboard.Listener(
-#     on_press=on_press,
-#     on_release=on_release)
-# listener.start()
-
 def on_click(x, y, button, pressed):
-    # print('{0} at {1}'.format('Pressed' if pressed else 'Released',(x, y)))
+    global keybind_pressed
+    if ( button == button.x2 ):
+        keybind_pressed = True
+    else:
+        keybind_pressed = False
     if not pressed:
         # Stop listener
         return False
-
-
-# # ...or, in a non-blocking fashion:
-# listener = mouse.Listener(on_click=on_click)
-# listener.start()
 
     
 type = Controller()
@@ -45,12 +20,26 @@ type = Controller()
 wordlist = open("wordlist.txt", "r")
 
 for line in wordlist:
-    with mouse.Listener(on_click=on_click) as listener:
-        listener.join()
-    for char in line:
-        type.press(char)
-        type.release(char)
+    while ( not keybind_pressed ):
+        with mouse.Listener(on_click=on_click) as listener:
+            listener.join()
+    if ( keybind_pressed ):
+        for char in line:
+            type.press(char)
+            type.release(char)
+            time.sleep(0.01)
+        type.press(Key.enter)
+
+        upper = True
+        for char in line:
+            if upper:
+                type.press(char.upper())
+                upper = False
+            else:
+                type.press(char)
+            type.release(char)
+            time.sleep(0.01)
+        type.press(Key.enter)
+
         time.sleep(0.01)
-        print(char)
-    type.press(Key.enter)
-    time.sleep(0.2)
+        keybind_pressed = False
